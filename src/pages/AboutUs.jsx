b324bec4921d5f6e30ce3587874f144e5c5f8f39
Lucas Rodriguez <guezlucas@gmail.com>
2024-08-11 23:15:11 -0300

    css y correciones en about

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -17,23 +17,19 @@ const AboutUs = () => {
       
       <div className="message-section">
         <p className="highlight-text">
-          Somos un grupo de pibes apasionados por la tecnología y la innovación, siempre buscando 
-          hacer cosas piolas que sean útiles y accesibles para todos. Nuestra misión es crear 
-          aplicaciones que te faciliten la vida, que sean intuitivas y estén al alcance de cualquiera.
+          Somos dos amigos y compañero de la carrera apasionados por la tecnología y la innovación, siempre en busca de crear 
+          cosas geniales que sean útiles y accesibles para todos y todas. Nuestra meta es poder desarrollar aplicaciones que 
+          te faciliten la vida, que sean intuitivas y estén al alcance de cualquiera.
         </p>
         <p className="highlight-text">
-          Nos criamos viendo Los Simpson, así que somos re fanáticos. Esa serie nos marcó desde que 
-          éramos pendejos hasta la adultez. Nos hizo reír, pensar y hasta nos enseñó cosas de la vida. 
-          Por eso, siempre hay un toque de ese humor y filosofía en todo lo que hacemos.
-        </p>
-        <p className="highlight-text">
-          Con años de experiencia en el desarrollo web, nos especializamos en armar aplicaciones bien 
-          robustas usando lo último en tecnología: React, Node.js, y bases de datos modernas, todo bien al día.
+        Crecimos viendo Los Simpson, así que somos fanáticos de corazón. Esta serie nos ha acompañado desde 
+        que éramos chicos hasta la adultez, haciéndonos reír, pensar y enseñándonos valiosas lecciones de vida. 
+        Por eso, siempre encontrarás un toque de ese humor y filosofía en todo lo que hacemos.
         </p>
       </div>
       
       <div className="link-section">
-        <Link to="/" className="back-home-link">Volver al inicio</Link>
+        <Link to="/" className="button is-link is-dark">Volver al inicio</Link>
       </div>
     </>
   );
